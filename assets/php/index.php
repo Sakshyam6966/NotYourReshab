68109c323068bec9d99f515c80c9f824715397be
Not Sakshyam <88571629+Sakshyam6966@users.noreply.github.com>
2021-09-09 16:49:47 +0000

    Initial commit

---
+++ assets/php/index.php
@@ -0,0 +1,13 @@
+<?php
+  $name = $_GET["name"];
+  $email = $_GET["email"];
+  $text = $_GET["textarea"];
+
+  $username = ;
+  $servername = ;
+  $password = ;
+  $conn = new mysqli($servername,$username,$password);
+
+  curl $REPLIT_DB_URL -d '<name>=<$name>';
+  echo "successfully";
+?>
